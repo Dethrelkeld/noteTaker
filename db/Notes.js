65f383610e267366eb145a9095f5a6d36b8d4956
DeThrelkeld <73961477+DeThrelkeld@users.noreply.github.com>
2021-02-02 16:58:16 -0600

    new files and methods in constructor

---
+++ db/Notes.js
@@ -0,0 +1,40 @@
+const util = require('util');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+const readFile = util.promisify(fs.readFile);
+
+const writeFile = util.promisify(fs.writeFile);
+
+class Notes {
+    read() {
+        return readFile('db/db.json', 'utf-8')
+    }
+    write(note) {
+        return writeFile('db/db.json', JSON.stringify(note), 'utf-8');
+    }
+    getNotes() {
+        return this.read().then(function (rawNotes) {
+            let readNotes = [];
+
+            try {
+                readNotes = readNotes.concat(JSON.parse(rawNotes))
+            } catch (error) {
+                readNotes = [];
+            } return readNotes
+        });
+    };
+    addNote(note) {
+        const newNote = {
+            title: note.title,
+            text: note.text,
+            id: uuidv4()
+        }
+
+        return this.getNotes().then(notesArray => [...notesArray, newNote]).then(goodNote => this.write(goodNote))
+
+    };
+
+}
+
+module.exports = new Notes() 
---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+
+const express = require('express');
+const path = require('path');
+const viewRoute = require("./routes/view");
+// Sets up the Express App
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// connect routes
+app.use("/", viewRoute)
+
+
+// start server
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
